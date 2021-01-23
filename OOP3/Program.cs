892fbbff786b11e13f2cf78a59bc3a1dca3c71e3
DELL <DELL@DESKTOP-L230P3N>
2021-01-23 23:56:56 +0300

    Add Project Files

--- OOP3/Program.cs
+++ OOP3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace OOP3
 {
@@ -6,7 +7,27 @@ namespace OOP3
     {
         static void Main(string[] args)
         {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            //ıhtiyacKrediManager.Hesapla();
+
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            //tasitKrediManager.Hesapla();
+
+            IKrediManager konutKrediManager = new KonutKrediManager();
+            //konutKrediManager.Hesapla();
+            ILoggerService databaseLoggerService = new DataBaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(tasitKrediManager, databaseLoggerService);
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager , tasitKrediManager };
+           // basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+
+
             Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
